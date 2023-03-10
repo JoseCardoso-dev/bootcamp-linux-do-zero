1d51cdfce1ad32149167f014381259e5a1287f5a
José Roberto Cardoso <jrcardoso541@gmail.com>
2023-03-09 21:24:44 -0300

    add script of installation my apps basic

---
+++ Scripts/IaC-my-dependency.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+sudo apt update && apt upgrade
+
+echo "-----------------------------------------------------"
+echo "Instalando o Braver..."
+
+sudo apt install curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
+
+echo "Braver Instalado!"
+echo "-----------------------------------------------------"
+
+echo "Instalando o Vs Code..."
+
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+echo "Vs Code Instalado!"
+echo "-----------------------------------------------------"
+
+echo "Instalando o Vlc...!"
+sudo snap install vlc
+echo "Vlc Instalado!"
+echo "-----------------------------------------------------"
+
+echo "Instalando o Discord...!"
+sudo snap install discord
+echo "Discord Instalado!"
+echo "-----------------------------------------------------"
+
+echo "Instalando o Docker...!"
+sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+echo "Docker Instalado!"
+echo "-----------------------------------------------------"
+
+
